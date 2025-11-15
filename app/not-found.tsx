c484cfc6727ac8b8329b1cb35b36ee9ed05a0cc8
HVRDWIRE <jaeklimited@gmail.com>
2025-11-15 15:41:33 +0000

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Noise from './Noise;'
+import Noise from './app/components/noise;'
 
 <div style={{width: '600px', height: '400px', position: 'relative', overflow: 'hidden'}}>
   <Noise
