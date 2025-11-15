febdfc1331d8bec71f8c36d3c14c5a85ffe925aa
HVRDWIRE <jaeklimited@gmail.com>
2025-11-15 15:38:14 +0000

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,15 @@
 "use client";
+import Noise from './Noise;'
+
+<div style={{width: '600px', height: '400px', position: 'relative', overflow: 'hidden'}}>
+  <Noise
+    patternSize={250}
+    patternScaleX={1}
+    patternScaleY={1}
+    patternRefreshInterval={2}
+    patternAlpha={15}
+  />
+</div>
 
 import ASCIIText from "./components/ASCIIText";
 
