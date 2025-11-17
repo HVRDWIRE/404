94a80502676115839f5f25217b9b67f347235d14
HVRDWIRE <jaeklimited@gmail.com>
2025-11-17 16:42:46 +0000

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,22 @@
 "use client";
-import Noise from './components/Noise'
-
-<div style={{width: '600px', height: '400px', position: 'relative', overflow: 'hidden'}}>
-  <Noise
-    patternSize={500}
-    patternScaleX={5}
-    patternScaleY={5}
-    patternRefreshInterval={1}
-    patternAlpha={25}
-  />
-</div>
-
+import Noise from './components/Noise';
 import ASCIIText from "./components/ASCIIText";
+import PillButton from "./components/PillButton";
 
 export default function NotFound() {
   return (
     <div className="relative flex min-h-screen items-center justify-center bg-black overflow-hidden">
+
+      <div style={{width: '600px', height: '400px', position: 'relative', overflow: 'hidden'}}>
+        <Noise
+          patternSize={500}
+          patternScaleX={5}
+          patternScaleY={5}
+          patternRefreshInterval={1}
+          patternAlpha={25}
+        />
+      </div>
+
       <div className="absolute inset-0">
         <ASCIIText
           text="404"
@@ -26,28 +27,24 @@ export default function NotFound() {
       </div>
 
       <div className="relative z-10 text-center">
-  <p className="mt-6 text-9xl font-mono" style={{ color: '#bbff22' }}>
-    Page not found
-  </p>
-</div>
+        <p className="mt-6 text-9xl font-mono" style={{ color: '#bbff22' }}>
+          Page not found
+        </p>
+      </div>
 
-      import PillButton from "./components/PillButton";
+      <div className="fixed bottom-8 left-0 right-0 flex justify-center gap-8 z-10">
 
-<div className="fixed bottom-8 left-0 right-0 flex justify-center gap-8 z-10">
+        <PillButton
+          modelPath="/assets/models/blue.fbx"
+          link="https://jæk.com"
+        />
 
-  <PillButton
-    modelPath="/assets/models/blue.fbx"
-    link="https://jæk.com"
-  />
+        <PillButton
+          modelPath="/assets/models/red.fbx"
+          link="https://jæk.com/resume"
+        />
 
-  <PillButton
-    modelPath="/assets/models/red.fbx"
-    link="https://jæk.com/resume"
-  />
-
-</div>
-
-      
+      </div>
     </div>
   );
 }
